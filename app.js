cbba1c9a05b61f421f66389f33e8b3b6f3d84fa6
是甜食哇 <54750485+shitianshiwa@users.noreply.github.com>
2020-09-16 16:40:29 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const fs = require('fs');
 const plugin = require('./plugin');
+/*const {
+    exit
+} = require('process');*/
+/*const db2 = require('./lib/db2');
 
-try{
-	fs.mkdirSync('./data');
-}catch (e) {}
+let id = parseInt(new Date().getTime() / 1000);
+db2.read().get(`rss[feed]`)
+    .push({
+        id: id,
+        url: "link2",
+        group: "group",
+        user: "sender",
+        status: "enable"
+    })
+    .write();
+console.log("2333");
+let data = db2.read().get(`rss[feed]`).find({ url: 'link3' }).value();
+console.log(data)
+process.exit()*/
+try {
+    fs.mkdirSync('./data');
+} catch (e) {}
 
 // 开始加载插件
 plugin();
