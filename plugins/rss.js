7caf072967b0a8ea6223995b528e2cd003107bcf
是甜食哇 <54750485+shitianshiwa@users.noreply.github.com>
2020-09-16 16:43:28 +0900

    Update rss.js

--- plugins/rss.js
+++ plugins/rss.js
@@ -1,159 +1,216 @@
 const rss = require('rss-parser');
 const admin = require('../lib/admin');
 const api = require('../lib/api')
-const db = require('../lib/db');
+const db2 = require('../lib/db2'); //json数据库
 
 const n = {};
 const parser = new rss()
 
-const update = async () => {
-	api.logger.info(`RSS 开始更新订阅`);
-	const r = await db.select('*').from('feed').where('status', 'enable').queryList();
-	const tmp = [];
-	for (const _rss of r) {
-		if(tmp.indexOf(_rss.url) === -1){
-			tmp.push(_rss.url);
-			parser.parseURL(_rss.url).then(async rss_result => {
-				const id = rss_result.items[0].link;
-				if(id !== _rss.last_id) {
-					const groups = await db.select('*').from('feed').where('url', _rss.url).queryList();
-					groups.forEach(e => {
-						api.bot.socket.send.group([
-							`[RSS] 您订阅的 ${rss_result.title.trim()} 更新了`,
-							`标题：${rss_result.items[0].title.trim()}`,
-							`链接：${rss_result.items[0].link}`
-						].join('\n'), e.group);
-					});
-					db.update('feed').where('url', _rss.url).column('last_id', id).execute();
-				}
-			}).catch(e => {
-				api.logger.warn(`RSS 更新失败, url: ${_rss.url}, err: ${JSON.stringify(e)}`);
-			})
-		}
-	}
-
-	api.logger.info(`RSS 订阅更新完成，跳过了 ${r.length - tmp.length} 个订阅`);
+const update = async() => {
+    api.logger.info(`RSS 开始更新订阅`);
+    const r = await db2.read().get(`rss[feed]`).value();
+    const tmp = [];
+    for (const _rss of r) {
+        if (tmp.indexOf(_rss.url) === -1) {
+            tmp.push(_rss.url);
+            parser.parseURL(_rss.url).then(async rss_result => {
+                const id = rss_result.items[0].link; //最新的
+                //console.log(rss_result);
+                //console.log(id)
+                //console.log(_rss.last_id)
+                let index = 0;
+                let i = 0;
+                let s = "";
+                for (i = 0; i < rss_result.items.length; i++) { //判断更新了多少条
+                    if (_rss.last_id == rss_result.items[i].link) {
+                        break;
+                    } else {
+                        index++;
+                    }
+                }
+                for (i = 0; i < index; i++) { //确认要更新多少后，开始转发
+                    s += [
+                        `[RSS] 您订阅的 ${rss_result.title.trim()} 更新了`,
+                        `标题：${rss_result.items[i].title.trim()}`,
+                        `链接：${rss_result.items[i].link}`
+                    ].join('\n');
+                }
+                //console.log(groups.group);
+                if (index > 0) { //有更新才转发
+                    const groups = await db2.read().get(`rss[feed]`).find({
+                        url: _rss.url
+                    }).value();
+                    api.bot.send.group(s, groups.group);
+                    db2.read().get(`rss[feed]`).find({
+                        url: _rss.url
+                    }).assign({
+                        last_id: id
+                    }).write();
+                }
+            }).catch(e => {
+                api.logger.warn(`RSS 更新失败, url: ${_rss.url}, err: ${JSON.stringify(e)}`);
+            })
+        }
+    }
+    api.logger.info(`RSS 订阅更新完成，跳过了 ${r.length - tmp.length} 个订阅`);
 }
 
 module.exports = {
-	plugin: {
-		name: 'RSS订阅器',
-		desc: 'RSS订阅器',
-		version: '0.0.1',
-		author: '涂山苏苏'
-	},
-	events: {
-		// 加载
-		onload: (e) => {
-			n.timer = setInterval(async () => {
-				await update();
-			}, 3e4);
-			api.logger.info(`RSS RSS订阅器开始运行`);
-		},
-		// 卸载
-		onunload: (e) => {
-			clearInterval(n.timer);
-			api.logger.info(`RSS RSS订阅器停止运行`);
-		}
-	},
-	commands: [
-		{
-			id: 'add',
-			helper: '.rss add [链接]	添加订阅',
-			command: /^\.rss add (.*)$/,
-			func: async (e) => {
-				const link = e.msg.substr(9);
-				const group = e.group;
-				const sender = e.sender.user_id;
+    plugin: {
+        name: 'RSS订阅器',
+        desc: 'RSS订阅器',
+        version: '0.0.1',
+        author: '涂山苏苏'
+    },
+    events: {
+        // 加载
+        onload: (e) => {
+            n.timer = setInterval(async() => {
+                await update();
+            }, 3e4);
+            api.logger.info(`RSS RSS订阅器开始运行`);
+        },
+        // 卸载
+        onunload: (e) => {
+            clearInterval(n.timer);
+            api.logger.info(`RSS RSS订阅器停止运行`);
+        }
+    },
+    commands: [{
+            id: 'add',
+            helper: '。rss add [链接]	添加订阅',
+            command: /^。rss add (.*)$/,
+            func: async(e) => {
+                const link = e.msg.substr(9);
+                /*
+ https://www.w3school.com.cn/js/jsref_substr.asp JavaScript substr() 方法
+参数	描述
+start	必需。要抽取的子串的起始下标。必须是数值。如果是负数，那么该参数声明从字符串的尾部开始算起的位置。也就是说，-1 指字符串中最后一个字符，-2 指倒数第二个字符，以此类推。
+length	可选。子串中的字符数。必须是数值。如果省略了该参数，那么返回从 stringObject 的开始位置到结尾的字串。
+ */
+                const group = e.group;
+                const sender = e.sender.user_id;
 
-				if(!admin.isAdmin(e.sender.user_id)){
-					api.bot.socket.send.group('¿', e.group);
-					return;
-				}
+                if (!admin.isAdmin(e.sender.user_id)) {
+                    api.bot.send.group('很抱歉，你不是机器人管理员，无权限操作！', e.group);
+                    return;
+                }
 
-				if(/^(http(s)?:\/\/)\w+[^\s]+(\.[^\s]+){1,}$/g.test(link)){
-					parser.parseURL(link).then(e => {
-						db
-						.insert('feed')
-						.column('url', link)
-						.column('group', group)
-						.column('user', sender)
-						.column('status', 'enable')
-						.execute();
-						api.bot.socket.send.group('[RSS] 订阅成功', group);
-					}).catch(e => {
-						api.bot.socket.send.group('[RSS] 订阅失败', group);
-					});
-				}else{
-					api.bot.socket.send.group('[RSS] 请填写正确的链接', group);
-				}
-			}
-		},
-		{
-			id: 'remove',
-			helper: '.rss remove [id]	删除订阅',
-			command: /^\.rss remove (.*)$/,
-			func: async (e) => {
-				const id = e.msg.substr(12);
-				const group = e.group;
-
-				if(!admin.isAdmin(e.sender.user_id)){
-					api.bot.socket.send.group('¿', e.group);
-					return;
-				}
-
-				try{
-					db
-					.delete('feed')
-					.where('id', id)
-					.where('group', group)
-					.execute();
-					api.bot.socket.send.group('[RSS] 删除成功', group);
-				}catch (e) {
-					api.bot.socket.send.group('[RSS] 删除失败', group);
-				}
-			}
-		},
-		{
-			id: 'list',
-			helper: '.rss list	查看本群订阅列表',
-			command: /\.rss list/,
-			func: async (e) => {
-				try{
-					const r = await db
-					.select('*')
-					.from('feed')
-					.where('group', e.group)
-					.queryList();
-
-					const m = [];
-					r.forEach(e => {
-						m.push(`${Number(e.id)}. ${e.url}`);
-					});
-
-					if(r.length === 0){
-						api.bot.socket.send.group('[RSS] 这个群还没有订阅任何内容', e.group);
-					}else{
-						api.bot.socket.send.group(m.join('\n'), e.group);
-					}
-				}catch (e) {
-					api.bot.socket.send.group('[RSS] 查询失败', e.group);
-				}
-			}
-		},
-		{
-			id: 'update',
-			helper: '.rss update	立刻刷新订阅',
-			command: /\.rss update/,
-			func: async (e) => {
-				if(!admin.isAdmin(e.sender.user_id)){
-					api.bot.socket.send.group('¿', e.group);
-					return;
-				}
-
-				await update();
-				api.bot.socket.send.group('[RSS] 刷新成功', e.group);
-			}
-		}
-	]
+                if (/^(http(s)?:\/\/)\w+[^\s]+(\.[^\s]+){1,}$/g.test(link)) {
+                    parser.parseURL(link).then(async e => {
+                        if (await db2.read().get(`rss[feed]`).find({
+                                url: link,
+                                group: group
+                            }).value() == undefined) {
+                            let id = parseInt(new Date().getTime() / 1000);
+                            await db2.read().get(`rss[feed]`)
+                                .push({
+                                    id: id,
+                                    url: link,
+                                    group: group,
+                                    user: sender,
+                                    status: "enable",
+                                    last_id: ""
+                                })
+                                .write();
+                            api.bot.send.group('[RSS] 订阅成功', group);
+                        } else {
+                            api.bot.send.group('[RSS] 该rss已订阅', group);
+                        }
+                    }).catch(e => {
+                        api.bot.send.group('[RSS] 订阅失败：' + e, group);
+                    });
+                } else {
+                    api.bot.send.group('[RSS] 请填写正确的链接', group);
+                }
+            }
+        },
+        {
+            id: 'remove',
+            helper: '。rss del [id]	删除订阅',
+            command: /^。rss del (.*)$/,
+            func: async(e) => {
+                const id = e.msg.substr(9);
+                const group = e.group;
+                //console.log(id);
+                if (!admin.isAdmin(e.sender.user_id)) {
+                    api.bot.send.group('很抱歉，你不是机器人管理员，无权限操作！', e.group);
+                    return;
+                }
+                try {
+                    //console.log(await db2.read().get(`rss[feed]`).find({
+                    //    id: parseInt(id)
+                    //}).value())
+                    if (await db2.read().get(`rss[feed]`).find({
+                            id: parseInt(id)
+                        }).value() != undefined) {
+                        await db2.read().get(`rss[feed]`)
+                            .remove({
+                                id: parseInt(id)
+                            })
+                            .write();
+                        api.bot.send.group('[RSS] 删除成功', group);
+                    } else {
+                        api.bot.send.group('[RSS] 该rss不存在，无法删除', group);
+                    }
+                } catch (e) {
+                    api.bot.send.group('[RSS] 删除失败:' + e, group);
+                }
+            }
+        },
+        {
+            id: 'list',
+            helper: '。rss list	查看本群订阅列表',
+            command: /。rss list/,
+            func: async(e) => {
+                try {
+                    let s1 = "";
+                    let data = db2.read().get(`rss[feed]`).value();
+                    //console.log(data);
+                    if (data.length != 0) {
+                        for (let i = 0; i < data.length; i++) {
+                            //console.log(data[i].id);
+                            //console.log(data[i].url);
+                            //console.log(data[i].group);
+                            //console.log(data[i].user);
+                            //console.log(data[i].status);
+                            s1 += "id: " + data[i].id + " , ";
+                            s1 += "url：" + data[i].url;
+                            //s2 += "group:" + data[i].group + "\n";
+                            //s2 += "user:" + data[i].user + "\n";
+                            //s2 += "status:" + data[i].status;
+                            s1 += "\n";
+                        }
+                        api.bot.send.group(s1, e.group);
+                        //console.log(s1);
+                    } else {
+                        api.bot.send.group('[RSS] 这个群还没有订阅任何内容', e.group);
+                    }
+                } catch (e) {
+                    api.bot.send.group('[RSS] 查询失败：' + e, e.group);
+                }
+            }
+        },
+        {
+            id: 'update',
+            helper: '。rss update	立刻刷新订阅',
+            command: /。rss update/,
+            func: async(e) => {
+                if (!admin.isAdmin(e.sender.user_id)) {
+                    api.bot.send.group('很抱歉，你不是机器人管理员，无权限操作！', e.group);
+                    return;
+                }
+                await update();
+                api.bot.send.group('[RSS] 刷新成功', e.group);
+            }
+        },
+        {
+            id: 'help',
+            helper: '。rss help	rss帮助说明',
+            command: /。rss help/,
+            func: async(e) => {
+                api.bot.send.group('[RSS] 指令列表：\n查询 。rss list\n增加 。rss add\n删除 。rss del\n立即刷新 。rss update', e.group);
+            }
+        }
+    ]
 }
