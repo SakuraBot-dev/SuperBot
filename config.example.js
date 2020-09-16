0a5f502987604fa7ffc971a805e252f4e4e11074
是甜食哇 <54750485+shitianshiwa@users.noreply.github.com>
2020-09-16 16:42:36 +0900

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,30 +1,32 @@
 module.exports = {
-	socket: {
-		api: '',
-		event: ''
-	},
-	db: {
-		// 数据库配置
-		host: '',
-		user: '',
-		database: '',
-		password: '',
-		port: 0
-	},
-	plugin: {
-		whois: {
-			token: ''		// 查询Whois的API
-		},
-		sec: {
-			cookie: ''		// 微步的cookie
-		},
-		vtb: {
-			host: 'https://api.vtbs.moe',		// socket.io的连接地址
-			groups: []		// vtb插件消息发送群号
-		}
-	},
-	logger: {
-		level: 'DEBUG',		// 日志等级
-	},
-	owner: ''			// 所属人QQ号
+    //正向ws
+    socket: {
+        api: 'ws://127.0.0.1:6700/ws/api/',
+        event: 'ws://127.0.0.1:6700/ws/event/'
+    },
+    db: {
+        // 数据库配置
+        host: '',
+        user: '',
+        database: '',
+        password: '',
+        port: 0
+    },
+    plugin: {
+        rss: {},
+        whois: {
+            token: '' // 查询Whois的API
+        },
+        sec: {
+            cookie: '' // 微步的cookie
+        },
+        vtb: {
+            host: 'https://api.vtbs.moe', // socket.io的连接地址
+            groups: [] // vtb插件消息发送群号
+        }
+    },
+    logger: {
+        level: 'DEBUG', // 日志等级
+    },
+    owner: '' // 所属人QQ号
 }
