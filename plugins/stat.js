f2d38e79ca3ddc785a603a477f8950d76ba9d0c5
是甜食哇 <54750485+shitianshiwa@users.noreply.github.com>
2020-09-17 08:05:11 +0900

    解决发送消息报错问题

--- plugins/stat.js
+++ plugins/stat.js
@@ -1,34 +1,32 @@
 const api = require('../lib/api');
 
 module.exports = {
-	plugin: {
-		name: 'stat',
-		desc: '消息统计',
-		version: '0.0.1',
-		author: '涂山苏苏'
-	},
-	events: {
-		// 加载
-		onload: (e) => {
-			api.logger.info('stat 开始运行')
-		},
-		// 卸载
-		onunload: (e) => {
-			api.logger.info('stat 停止始运行')
-		}
-	},
-	commands: [
-		{
-			id: 'stat',
-			helper: '.stat 查看发送/接收消息数量',
-			command: /^\.stat$/,
-			func: async (e) => {
-				api.bot.socket.send.group([
-					`接收消息: ${api.bot.stat.receive || 'unknown'} 条`,
-					`发送消息: ${api.bot.stat.send || 'unknown'} 条`,
-					`raw消息: ${api.bot.stat.raw || 'unknown'} 条`
-				].join('\n'), e.group);
-			}
-		}
-	]
+    plugin: {
+        name: 'stat',
+        desc: '消息统计',
+        version: '0.0.1',
+        author: '涂山苏苏'
+    },
+    events: {
+        // 加载
+        onload: (e) => {
+            api.logger.info('stat 开始运行')
+        },
+        // 卸载
+        onunload: (e) => {
+            api.logger.info('stat 停止始运行')
+        }
+    },
+    commands: [{
+        id: 'stat',
+        helper: '.stat 查看发送/接收消息数量',
+        command: /^\.stat$/,
+        func: async(e) => {
+            api.bot.send.group([
+                `接收消息: ${api.bot.stat.receive || 'unknown'} 条`,
+                `发送消息: ${api.bot.stat.send || 'unknown'} 条`,
+                `raw消息: ${api.bot.stat.raw || 'unknown'} 条`
+            ].join('\n'), e.group);
+        }
+    }]
 }
